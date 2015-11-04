66100ab2e0234f3e3f5ecf6a4c981724bac133c6
Guillaume <admin@guillaume-leduc.fr>
2015-11-04 20:45:13 +0100

    Fix #133

--- app/Model/User.php
+++ app/Model/User.php
@@ -146,7 +146,12 @@ class User extends AppModel {
 
     public function isThereAnAdmin() {
         if (AuthComponent::user('id') == $this->data[$this->alias]['id'] && isset($this->data[$this->alias]['role'])) {
-            return false;
+
+            $users = $this->find('count');
+
+            if ($users > 0) {
+                return false;
+            }
         }
         return true;
     }
